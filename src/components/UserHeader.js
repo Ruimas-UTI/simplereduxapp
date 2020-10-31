a261dbcb0de0463cffc9192da3c40e4bf12aafd7
Ruimas-UTI <allpraisethelord@gmail.com>
2020-10-31 14:41:38 +0600

    debugged

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -6,14 +6,14 @@ class UserHeader extends React.Component {
         this.props.fetchUser(this.props.userId);
     }
     render(){
-        const user = this.props.user.find((user)=> user.id === this.props.userId);
+        const { user } = this.props;
         if (!user){
             return null;
         }
         return <div className="header">{user.name}</div>
     };
 };
-const mapStateToProps = state => {
-    return {user: state.users}
+const mapStateToProps = (state, ownProps) => {
+    return {user: state.users.find((user)=> user.id === ownProps.userId)}
 }
 export default connect(mapStateToProps, {fetchUser})(UserHeader);
